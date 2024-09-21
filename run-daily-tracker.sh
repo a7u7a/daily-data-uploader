48a48ff1ac393fe520731145bc9365212a444dee
a7u7a <esteban@esrs.co>
2024-09-21 22:27:03 +0200

    Remove run-daily-tracker.sh from Git tracking

--- run-daily-tracker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DAILY_TRACKER_EXPORT_DIR="/Users/userfriendly/Documents/daily-export-data"
-export DAILY_TRACKER_LOG_DIR="$DAILY_TRACKER_EXPORT_DIR/logs"
-
-# Ensure the log directory exists
-mkdir -p "$DAILY_TRACKER_LOG_DIR"
-
-/usr/bin/python3 ./app.py
